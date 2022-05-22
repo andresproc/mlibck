fe19866d21c146a231d655a2455b32f0e37be6bd
andresproc <44841127+andresproc@users.noreply.github.com>
2022-05-22 12:28:15 -0500

    Create File1.py
    Comentario

---
+++ File1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import io
+import numpy as np
+from sklearn import datasets,linear_model
+import matplotlib.pyplot as plt
+
+
+Salarios = pd.read_excel('BaseML3.xlsx')
+
+x = Salarios['Expe']
+X = x[:,np.newaxis]
+y= np.array(ASIS['Salario'])
+
+
+plt.scatter(X,y)
+plt.xlabel('Años Exp')
+plt.ylabel('Salario')
+plt.show()
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+lr = linear_model.LinearRegression()
+lr.fit(X_train,y_train)
+Y_pred = lr.predict(X_test)
+
+
+plt.scatter(X_test,y_test)
+plt.plot(X_test,Y_pred,color='red',linewidth=3)
+plt.xlabel('Años Exp')
+plt.ylabel('Salario')
+plt.savefig('grafica.png')
+plt.show()
+
+import pickle
+
+pickle.dump(lr, open("modelo.pkl", 'wb'))
